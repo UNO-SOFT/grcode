c28921677eda20c711150f3ce1772ee535aad893
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2023-06-11 13:29:18 +0200

    add go.mod and make scanner.Scan return the error

--- scanner.go
+++ scanner.go
@@ -4,6 +4,11 @@ package grcode
 // #include <zbar.h>
 import "C"
 
+import (
+	"strconv"
+	"unsafe"
+)
+
 // Scanner is wrapper of zbar_image_scanner
 type Scanner struct {
 	image_scanner *C.zbar_image_scanner_t
@@ -28,13 +33,40 @@ func (s *Scanner) SetConfig(symbology C.zbar_symbol_type_t, config C.zbar_config
 // Scan scans image, the image format must be "Y800" or "GRAY".
 // returns:
 // >0, if symbols were successfully decoded from the image
-//  0, if no symbols were found or
+//
+//	0, if no symbols were found or
+//
 // -1, if an error occurs
-func (s *Scanner) Scan(img *ZbarImage) int {
-	return int(C.zbar_scan_image(s.image_scanner, img.image))
+func (s *Scanner) Scan(img *ZbarImage) (int, error) {
+	rc := int(C.zbar_scan_image(s.image_scanner, img.image))
+	if rc < 0 {
+		return rc, &zbarError{
+			Code:    int(C.int(C._zbar_get_error_code(unsafe.Pointer(s.image_scanner)))),
+			Message: C.GoString(C._zbar_error_string(unsafe.Pointer(s.image_scanner), 1)),
+		}
+	}
+	return rc, nil
 }
 
 // Close suicides
 func (s *Scanner) Close() {
 	C.zbar_image_scanner_destroy(s.image_scanner) // void function
 }
+
+type zbarError struct {
+	Code    int
+	Message string
+}
+
+func (ze *zbarError) Error() string {
+	if ze == nil {
+		return ""
+	}
+	b := make([]byte, 0, 128)
+	b = strconv.AppendInt(b, int64(ze.Code), 10)
+	b = append(b, ": "...)
+	b = append(b, ze.Message...)
+	return string(b)
+}
+
+var _ = error((*zbarError)(nil))
